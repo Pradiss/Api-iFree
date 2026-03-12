ed83592d121250f72e679f07c3440937d5656e25
Erick Prado <149429488+Pradiss@users.noreply.github.com>
2026-03-12 23:21:54 +0000

    Update app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,7 +4,7 @@ const app = require('./server')
 const { connectionBank } = require("./models")
 
 const PORT = parseInt(`${process.env.PORT || 3000}`)
-connectionBank.sync().then(() => {
+connectionBank.sync({ force: true}).then(() => {
     app.listen(PORT, () => 
         console.log(`🚀 Server running on http://localhost:${PORT}`)
     );
